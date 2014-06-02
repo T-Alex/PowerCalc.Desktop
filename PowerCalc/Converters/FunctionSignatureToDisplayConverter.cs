55a8e51d9c9ece1cc8b2a2c10291d648ce004b3a
Alex Titarenko <alex.titarenko@talex-soft.com>
2014-06-03 00:41:09 +0300

    ReferencesWindow: function section appearance has been changed.

--- PowerCalc/Converters/FunctionSignatureToDisplayConverter.cs
+++ PowerCalc/Converters/FunctionSignatureToDisplayConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,44 +15,37 @@ namespace TAlex.PowerCalc.Converters
 {
     public class FunctionSignatureToDisplayConverter : ConverterBase<FunctionSignatureToDisplayConverter>
     {
-        private static SolidColorBrush SignatureNameBrush = new SolidColorBrush(Color.FromRgb(220, 50, 47));
-        private static SolidColorBrush PuncBrush = new SolidColorBrush(Color.FromRgb(147, 161, 161));
-        private static SolidColorBrush TypeBrush = new SolidColorBrush(Colors.Teal);
-        private static SolidColorBrush ArgNameBrush = new SolidColorBrush(Color.FromRgb(25, 95, 145));
+        private static readonly SolidColorBrush SignatureNameBrush = new SolidColorBrush(Color.FromRgb(220, 50, 47));
+        private static readonly SolidColorBrush PuncBrush = new SolidColorBrush(Color.FromRgb(147, 161, 161));
+        private static readonly SolidColorBrush TypeBrush = new SolidColorBrush(Colors.Teal);
+        private static readonly SolidColorBrush ArgNameBrush = new SolidColorBrush(Color.FromRgb(25, 95, 145));
 
 
-        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             FunctionSignature signature = value as FunctionSignature;
 
             TextBlock textBlock = new TextBlock();
-            textBlock.Inlines.Add(new Run(signature.Name) { Foreground = SignatureNameBrush });
-            textBlock.Inlines.Add(new Run("(") { Foreground = PuncBrush });
+            InlineCollection inlines = textBlock.Inlines;
 
-            textBlock.Inlines.AddRange(signature.Arguments.SelectMany(a => new List<Inline> { new Run(a.Type) { Foreground = TypeBrush }, new Run(" "), new Run(a.Name) { Foreground = ArgNameBrush } }));
+            inlines.Add(new Run(signature.Name) { Foreground = SignatureNameBrush });
+            inlines.Add(new Run("(") { Foreground = PuncBrush });
 
-            //for (int i = 0; i < signatures.Count; i++)
-            //{
-            //    SignatureViewModel signature = signatures[i];
-            //    textBlock.Inlines.Add(String.Format("{0}(", signature.Name));
+            for (int i = 0; i < signature.Arguments.Count; i++)
+            {
+                var arg = signature.Arguments[i];
 
-            //    for (int j = 0; j < signature.Arguments.Count; j++)
-            //    {
-            //        KeyValuePair<string, string> arg = signature.Arguments[j];
+                inlines.Add(new Run(arg.Type) { Foreground = TypeBrush });
+                inlines.Add(new Run(" "));
+                inlines.Add(new Run(arg.Name) { Foreground = ArgNameBrush });
 
-            //        textBlock.Inlines.Add(new Italic(new Run(arg.Key)));
-            //        textBlock.Inlines.Add(new Run(" " + arg.Value + ((j < signature.Arguments.Count - 1) ? ", " : String.Empty)));
-            //    }
+                if (i < signature.Arguments.Count - 1)
+                {
+                    inlines.Add(new Run(", ") { Foreground = PuncBrush });
+                }
+            }
 
-            //    textBlock.Inlines.Add(")");
-
-            //    if (i < signatures.Count - 1)
-            //    {
-            //        textBlock.Inlines.Add(new LineBreak());
-            //    }
-            //}
-
-            textBlock.Inlines.Add(new Run(")") { Foreground = PuncBrush });
+            inlines.Add(new Run(")") { Foreground = PuncBrush });
 
             return textBlock;
         }
