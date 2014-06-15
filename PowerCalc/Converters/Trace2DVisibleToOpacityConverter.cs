eda8773839dc50f64e8aea71e4425f18ccbc1f6c
Alex Titarenko <alex.titarenko@talex-soft.com>
2014-06-15 12:10:37 +0300

    MathCore.ExpressionsBase package has been update to the latest version, ListBoxItem style has been changed, Traces2DList appearance has been changed.

---
+++ PowerCalc/Converters/Trace2DVisibleToOpacityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAlex.WPF.Converters;
+
+
+namespace TAlex.PowerCalc.Converters
+{
+    public class Trace2DVisibleToOpacityConverter: ConverterBase<Trace2DVisibleToOpacityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? 1 : 0.4;
+        }
+    }
+}
